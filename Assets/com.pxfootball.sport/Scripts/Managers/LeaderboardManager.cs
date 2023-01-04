fe6ce57ca5cb1a31dd2ea0d1c97bbaeba83b9315
Sladkiy Eduard <nikcxprojects@gmail.com>
2023-01-04 17:51:10 +0300

    update

--- Assets/com.pxfootball.sport/Scripts/Managers/LeaderboardManager.cs
+++ Assets/com.pxfootball.sport/Scripts/Managers/LeaderboardManager.cs
@@ -13,30 +13,15 @@ public class LeaderboardManager : MonoBehaviour
         int[] scores = new int[container.childCount];
         for(int i = 0; i < scores.Length; i++)
         {
-            scores[i] = Random.Range(4500, 9999);
-        }
-
-        int[] times = new int[container.childCount];
-        for (int i = 0; i < times.Length; i++)
-        {
-            times[i] = Random.Range(150, 600);
+            scores[i] = Random.Range(2500, 6666);
         }
 
         var sortedScores = scores.OrderByDescending(i => i).ToArray();
         for(int i = 0; i < container.childCount; i++)
         {
-            Text leader = container.GetChild(i).GetChild(1).GetComponentInChildren<Text>();
+            int index = i < 3 ? 0 : 1;
+            Text leader = container.GetChild(i).GetChild(index).GetComponentInChildren<Text>();
             leader.text = string.Format("{0:0000}", sortedScores[i]);
-        }
-
-        for (int i = 0; i < container.childCount; i++)
-        {
-            Text timeText = container.GetChild(i).GetChild(2).GetComponentInChildren<Text>();
-
-            int min = Mathf.FloorToInt(times[i] / 60);
-            int sec = Mathf.FloorToInt(times[i] % 60);
-
-            timeText.text = string.Format("{0:00}:{1:00}", min, sec);
         }
     }
 }
